e5864d914a9d059cd6ee60fcc10edd3fdfb7f744
joe-qodes <jquao04@gmail.com>
2026-01-25 23:07:22 +0000

    fixed views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -77,3 +77,18 @@ def secure_view(request):
     response = render(request, "bookshelf/book_list.html")
     response["Content-Security-Policy"] = "default-src 'self'"
     return response
+
+
+from django.shortcuts import render
+from .forms import ExampleForm
+
+
+def example_form_view(request):
+    form = ExampleForm()
+
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            pass
+
+    return render(request, "bookshelf/form_example.html", {"form": form})
