d082b11d6eb582d13f707e9f406339b12bd81ae1
joe-qodes <jquao04@gmail.com>
2026-01-25 23:05:39 +0000

    fixed forms.py file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title", "author", "published_date"]
+
+class ExampleForm(forms.Form):
+    """
+    Example form used to demonstrate secure form handling
+    and CSRF protection.
+    """
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea, required=False)
