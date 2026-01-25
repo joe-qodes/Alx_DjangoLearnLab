c5221621a0ef4333d75ddf8772bdaaf69824e980
joe-qodes <jquao04@gmail.com>
2026-01-25 22:27:53 +0000

    Implemented a custom user model in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/update.md
@@ -0,0 +1,18 @@
+
+---
+
+# `update.md`
+
+```markdown
+# Update Book Instance
+
+## Django Shell Command
+
+```python
+from bookshelf.models import Book
+
+book = Book.objects.get(title="1984")
+book.title = "Nineteen Eighty-Four"
+book.save()
+
+book.title
---
+++ advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,22 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+# Query 1: Query all books by a specific author
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+
+# Query 2: List all books in a library
+def query_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
+
+# Query 3: Retrieve the librarian for a library
+def query_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    return librarian
