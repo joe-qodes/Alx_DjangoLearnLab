723c12588a7af8ce9216894a013ead471b79590d
joe-qodes <jquao04@gmail.com>
2026-02-01 20:11:11 +0000

    Fixed some wordings to meet checker requirements

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,9 +6,6 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Old endpoint (read-only)
     path('books/', BookList.as_view(), name='book-list'),
-
-    # Router endpoints (CRUD)
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
