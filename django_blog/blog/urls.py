eeab08bf12ec37f34cc356a8360d57f9bf84867e
joe-qodes <jquao04@gmail.com>
2026-02-15 17:20:06 +0000

    Fixed postsbytag() functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 from .views import (
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
-    CommentCreateView, CommentUpdateView, CommentDeleteView
+    CommentCreateView, CommentUpdateView, CommentDeleteView, PostByTagListView
 )
 
 urlpatterns = [
@@ -28,5 +28,5 @@ urlpatterns = [
     path("", views.home_view, name="home"),
 
     path('search/', views.search_posts, name='search-posts'),
-    path('tags/<slug:tag_slug>/', views.posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
